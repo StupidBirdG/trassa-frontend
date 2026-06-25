03c1461c1d8d82239c751330f23ba62702c9e849
StupidBirdG <46608789+StupidBirdG@users.noreply.github.com>
2026-06-25 15:09:01 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-const express = require("express");
-const { createProxyMiddleware } = require("http-proxy-middleware");
-const path = require("path");
+import express from "express";
+import { createProxyMiddleware } from "http-proxy-middleware";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use("/api", createProxyMiddleware({
   target: "https://trassa-backend-production.up.railway.app",
-    changeOrigin: true,
-      secure: true
-      }));
-      app.use(express.static(path.join(__dirname, "dist")));
-      app.get("*", (_, res) => res.sendFile(path.join(__dirname, "dist", "index.html")));
-      app.listen(PORT, () => console.log("Frontend on port " + PORT));
+  changeOrigin: true,
+  secure: true
+}));
+app.use(express.static(join(__dirname, "dist")));
+app.get("*", (_, res) => res.sendFile(join(__dirname, "dist", "index.html")));
+app.listen(PORT, () => console.log("Frontend on port " + PORT));
